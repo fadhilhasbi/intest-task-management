326aa0258a9023e5e2df9ef75eca7de4cce723b8
fadhilhasbi <muhammad1900016050@webmail.uad.ac.id>
2024-12-16 22:52:31 +0700

    chore: fix vercel.json 24

--- config/app.php
+++ config/app.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://intest-task-management.vercel.app'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
